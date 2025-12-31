ad5097aa341b99b5c73d010e5265ff48862da9b6
1robie <clarence@aheritier.net>
2025-12-31 14:41:31 +0100

    refactor: improve message loader with per-language file support and dynamic message validation

--- Plugin/src/main/java/fr/robie/craftengineconverter/loader/MessageLoader.java
+++ Plugin/src/main/java/fr/robie/craftengineconverter/loader/MessageLoader.java
@@ -1,223 +1,215 @@
 package fr.robie.craftengineconverter.loader;
 
+import fr.robie.craftengineconverter.common.CraftEngineConverterPlugin;
+import fr.robie.craftengineconverter.common.Manageable;
+import fr.robie.craftengineconverter.common.ObjectUtils;
+import fr.robie.craftengineconverter.common.configuration.Configuration;
+import fr.robie.craftengineconverter.common.enums.Languages;
 import fr.robie.craftengineconverter.common.format.Message;
 import fr.robie.craftengineconverter.common.format.MessageType;
+import fr.robie.craftengineconverter.common.logger.LogType;
 import fr.robie.craftengineconverter.common.logger.Logger;
-import fr.robie.craftengineconverter.utils.YamlUtils;
-import fr.robie.craftengineconverter.utils.save.Persist;
-import fr.robie.craftengineconverter.utils.save.Savable;
+import fr.robie.craftengineconverter.utils.SnakeUtils;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.io.InputStream;
+import java.util.*;
 
-public class MessageLoader extends YamlUtils implements Savable {
-    private final List<Message> loadedMessages = new ArrayList<>();
+public class MessageLoader extends ObjectUtils implements Manageable {
+    private static final String TRANSLATIONS_PATH = "translations/";
+    private static final String MESSAGES_FILE = "/messages.yml";
+    private static final String VERSION_KEY = "version";
 
-    public MessageLoader(Plugin plugin) {
-        super(plugin);
+    private final CraftEngineConverterPlugin plugin;
+    private final int version = 0;
+
+    public MessageLoader(CraftEngineConverterPlugin plugin) {
+        this.plugin = plugin;
     }
 
     @Override
-    public void save(Persist persist) {
+    public void reload() {
+        for (Languages lang : Languages.values()) {
+            String path = getLanguagePath(lang);
+            File file = new File(this.plugin.getDataFolder(), path);
 
-        if (persist != null) return;
-
-        File file = new File(plugin.getDataFolder(), "messages.yml");
-        if (!file.exists()) {
             try {
-                file.createNewFile();
-            } catch (IOException exception) {
-                Logger.showException("Error while creating messages.yml file", exception);
-                return;
+                ensureFileExists(file, path);
+                updateLanguageFile(file, path, lang);
+            } catch (Exception e) {
+                Logger.showException("Failed to process language file: " + path, e);
             }
         }
+        loadLanguage(Configuration.language);
+    }
 
-        YamlConfiguration configuration = getConfig(file);
-        for (Message message : Message.values()) {
+    private void ensureFileExists(File file, String path) {
+        if (!file.exists()) {
+            this.plugin.saveResource(path, false);
+        }
+    }
 
-            if (!message.isUse()) continue;
+    private void updateLanguageFile(File file, String path, Languages lang) throws Exception {
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        Object fileVersion = config.get(VERSION_KEY);
 
-            String path = message.name().toLowerCase().replace("_", "-");
-
-            if (configuration.contains(path)) continue;
-
-            if (message.getType() != MessageType.TCHAT) {
-                configuration.set(path + ".type", message.getType().name());
+        if (!(fileVersion instanceof Integer intVersion) || intVersion < this.version) {
+            config.set(VERSION_KEY, this.version);
+            if (updateMissingKeys(config, path, lang)) {
+                config.save(file);
+                Logger.info("Language file updated for language: " + lang.name(), LogType.INFO);
             }
-            if (message.getType().equals(MessageType.TCHAT) || message.getType().equals(MessageType.ACTION) || message.getType().equals(MessageType.CENTER)) {
+        }
+    }
 
-                if (message.isMessage()) {
-                    if (message.getType() != MessageType.TCHAT) {
-                        configuration.set(path + ".messages", colorReverse(message.getMessages()));
-                    } else {
-                        configuration.set(path, colorReverse(message.getMessages()));
-                    }
-                } else {
-                    if (message.getType() != MessageType.TCHAT) {
-                        configuration.set(path + ".message", colorReverse(message.getMessage()));
-                    } else {
-                        configuration.set(path, colorReverse(message.getMessage()));
+    private boolean updateMissingKeys(YamlConfiguration config, String path, Languages lang) throws Exception {
+        boolean updated = false;
+        try (InputStream inputStream = this.plugin.getResource(path)) {
+            if (inputStream == null) {
+                Logger.info("Language file not found in resources: " + path);
+                return updated;
+            }
+
+            try (SnakeUtils reader = new SnakeUtils(inputStream)) {
+                for (Message message : Message.values()) {
+                    String key = enumNameToKey(message.name());
+
+                    if (!config.contains(key)) {
+                        if (reader.contains(key)) {
+                            config.set(key,reader.getObject(key));
+                        } else {
+                            Logger.info("Missing message key for language " + lang.name() + ": " + key+ ". Please report this. Using default message.",LogType.WARNING);
+                            MessageType type = message.getType();
+                            if (type != MessageType.TCHAT) config.set(key + ".type", type.name());
+                            if (type == MessageType.TITLE){
+                                config.set(key + ".title", message.getTitle());
+                                config.set(key + ".subtitle", message.getSubTitle());
+                                config.set(key + ".fade-in", message.getStart());
+                                config.set(key + ".show-time", message.getTime());
+                                config.set(key + ".fade-out", message.getEnd());
+                            } else {
+                                if (message.isMessage()) {
+                                    config.set(key + (type != MessageType.TCHAT ? ".messages" : ""), message.getMessages());
+                                } else {
+                                    config.set(key + (type != MessageType.TCHAT ? ".message" : ""), message.getMessage());
+                                }
+                            }
+                        }
+                        updated = true;
                     }
                 }
-            } else if (message.getType().equals(MessageType.TITLE)) {
-
-                configuration.set(path + ".title", colorReverse(message.getTitle()));
-                configuration.set(path + ".subtitle", colorReverse(message.getSubTitle()));
-                configuration.set(path + ".fadeInTime", message.getStart());
-                configuration.set(path + ".showTime", message.getTime());
-                configuration.set(path + ".fadeOutTime", message.getEnd());
             }
         }
-
-        try {
-            configuration.save(file);
-        } catch (IOException exception) {
-            Logger.showException("Error while saving messages.yml file", exception);
-        }
-
-        loadMessages(configuration);
+        return updated;
     }
 
-    /**
-     * Loads messages from the configuration file.
-     *
-     * @param persist The persist instance used for loading the data.
-     */
-    @Override
-    public void load(Persist persist) {
+    public void loadLanguage(Languages language) {
+        File file = new File(this.plugin.getDataFolder(), getLanguagePath(language));
 
-        File file = new File(plugin.getDataFolder(), "messages.yml");
         if (!file.exists()) {
-            this.save(null);
+            Logger.info("Language file not found: " + file.getPath(), LogType.WARNING);
             return;
         }
 
-        YamlConfiguration configuration = getConfig(file);
-        this.save(null);
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        Set<String> keys = config.getKeys(true);
+        List<Message> loadedMessages = new ArrayList<>();
 
-        loadMessages(configuration);
-    }
+        for (String key : keys) {
+            if (VERSION_KEY.equals(key) || config.isConfigurationSection(key)) continue;
 
-    private void loadMessages(YamlConfiguration configuration) {
-
-        this.loadedMessages.clear();
-
-        for (String key : configuration.getKeys(false)) {
-            loadMessage(configuration, key);
-        }
-
-        boolean canSave = false;
-        for (Message message : Message.values()) {
-
-            if (!this.loadedMessages.contains(message) && message.isUse()) {
-                canSave = true;
-                break;
+            try {
+                Message message = parseMessage(key);
+                loadMessageContent(message, config, key);
+                loadedMessages.add(message);
+            } catch (IllegalArgumentException e) {
+                Logger.info("Unknown message key: " + key, LogType.WARNING);
+            } catch (Exception e) {
+                Logger.showException("Failed to load message: " + key, e);
             }
         }
 
-        if (canSave) {
-            this.save(null);
+        validateLoadedMessages(loadedMessages, language);
+    }
+
+    private Message parseMessage(String key) {
+        return Message.valueOf(keyToEnumName(key));
+    }
+
+    private void loadMessageContent(Message message, YamlConfiguration config, String key) {
+        if (config.contains(key + ".type")) {
+            loadTypedMessage(message, config, key);
+        } else {
+            loadSimpleMessage(message, config, key);
         }
     }
 
-    /**
-     * Loads a single message from the given YAML configuration.
-     *
-     * @param configuration The YAML configuration to load the message from.
-     * @param key           The key under which the message is stored.
-     */
-    private void loadMessage(YamlConfiguration configuration, String key) {
-        try {
+    private void loadTypedMessage(Message message, YamlConfiguration config, String key) {
+        MessageType messageType = MessageType.valueOf(
+                config.getString(key + ".type", "TCHAT").toUpperCase()
+        );
+        message.setType(messageType);
 
-            Message message = Message.valueOf(key.toUpperCase().replace("-", "_"));
-
-            if (configuration.contains(key + ".type")) {
-
-                MessageType messageType = MessageType.valueOf(configuration.getString(key + ".type", "TCHAT").toUpperCase());
-                message.setType(messageType);
-                switch (messageType) {
-                    case ACTION:
-                    case TCHAT_AND_ACTION: {
-                        message.setMessage(configuration.getString(key + ".message"));
-                        break;
-                    }
-                    case CENTER:
-                    case TCHAT:
-                    case WITHOUT_PREFIX: {
-                        List<String> messages = configuration.getStringList(key + ".messages");
-                        if (messages.isEmpty()) {
-                            message.setMessage(configuration.getString(key + ".message"));
-                        } else message.setMessages(messages);
-                        break;
-                    }
-                    case TITLE: {
-                        String title = configuration.getString(key + ".title");
-                        String subtitle = configuration.getString(key + ".subtitle");
-                        int fadeInTime = configuration.getInt(key + ".fadeInTime");
-                        int showTime = configuration.getInt(key + ".showTime");
-                        int fadeOutTime = configuration.getInt(key + ".fadeOutTime");
-                        Map<String, Object> titles = new HashMap<>();
-                        titles.put("title", color(title));
-                        titles.put("subtitle", color(subtitle));
-                        titles.put("start", fadeInTime);
-                        titles.put("time", showTime);
-                        titles.put("end", fadeOutTime);
-                        titles.put("isUse", true);
-                        message.setTitles(titles);
-                        break;
-                    }
-                }
-
-            } else {
-                message.setType(MessageType.TCHAT);
-                List<String> messages = configuration.getStringList(key);
-                if (messages.isEmpty()) {
-                    message.setMessage(configuration.getString(key));
-                } else message.setMessages(messages);
-            }
-
-            this.loadedMessages.add(message);
-        } catch (Exception ignored) {
+        switch (messageType) {
+            case ACTION, TCHAT_AND_ACTION -> loadActionMessage(message, config, key);
+            case CENTER, TCHAT, WITHOUT_PREFIX -> loadTextMessage(message, config, key);
+            case TITLE -> loadTitleMessage(message, config, key);
         }
     }
 
-    protected String color(String message) {
-        if (message == null) {
-            return null;
-        }
-        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
-        Matcher matcher = pattern.matcher(message);
-        while (matcher.find()) {
-            String color = message.substring(matcher.start(), matcher.end());
-            message = message.replace(color, String.valueOf(net.md_5.bungee.api.ChatColor.of(color)));
-            matcher = pattern.matcher(message);
-        }
-
-        return net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', message);
+    private void loadActionMessage(Message message, YamlConfiguration config, String key) {
+        message.setMessage(config.getString(key + ".message"));
     }
-    protected String colorReverse(String message) {
-        Pattern pattern = Pattern.compile(net.md_5.bungee.api.ChatColor.COLOR_CHAR + "x[a-fA-F0-9-" + net.md_5.bungee.api.ChatColor.COLOR_CHAR + "]{12}");
-        Matcher matcher = pattern.matcher(message);
-        while (matcher.find()) {
-            String color = message.substring(matcher.start(), matcher.end());
-            String colorReplace = color.replace("§x", "#");
-            colorReplace = colorReplace.replace("§", "");
-            message = message.replace(color, colorReplace);
-            matcher = pattern.matcher(message);
-        }
 
-        return message == null ? null : message.replace("§", "&");
+    private void loadTextMessage(Message message, YamlConfiguration config, String key) {
+        List<String> messages = config.getStringList(key + ".messages");
+        if (messages.isEmpty()) {
+            message.setMessage(config.getString(key + ".message"));
+        } else {
+            message.setMessages(messages);
+        }
     }
-    protected List<String> colorReverse(List<String> messages) {
-        return messages.stream().map(this::colorReverse).collect(Collectors.toList());
+
+    private void loadTitleMessage(Message message, YamlConfiguration config, String key) {
+        Map<String, Object> titles = new HashMap<>();
+        titles.put("title", config.getString(key + ".title"));
+        titles.put("subtitle", config.getString(key + ".subtitle"));
+        titles.put("start", config.getInt(key + ".fade-in", 10));
+        titles.put("time", config.getInt(key + ".show-time", 70));
+        titles.put("end", config.getInt(key + ".fade-out", 20));
+        titles.put("isUse", true);
+        message.setTitles(titles);
+    }
+
+    private void loadSimpleMessage(Message message, YamlConfiguration config, String key) {
+        message.setType(MessageType.TCHAT);
+        List<String> messages = config.getStringList(key);
+        if (messages.isEmpty()) {
+            message.setMessage(config.getString(key));
+        } else {
+            message.setMessages(messages);
+        }
+    }
+
+    private void validateLoadedMessages(List<Message> loadedMessages, Languages language) {
+        if (loadedMessages.size() != Message.values().length) {
+            Logger.info(String.format(
+                    "Loaded messages (%d) do not match expected count (%d) for language %s",
+                    loadedMessages.size(), Message.values().length, language.name()
+            ), LogType.WARNING);
+        }
+    }
+
+    private String getLanguagePath(Languages language) {
+        return TRANSLATIONS_PATH + language.name().toLowerCase() + MESSAGES_FILE;
+    }
+
+    private String keyToEnumName(String key) {
+        return key.toUpperCase().replace(".", "__").replace("-", "_");
+    }
+
+    private String enumNameToKey(String enumName) {
+        return enumName.toLowerCase().replace("__", ".").replace("_", "-");
     }
 }
